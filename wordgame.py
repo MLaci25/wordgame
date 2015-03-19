6f970cb9976622bdf4c60da76ae6a26c00599da3
MLaci25 <C00157639@itcarlow.ie>
2015-03-19 12:49:55 +0000

    Host

--- wordgame.py
+++ wordgame.py
@@ -162,5 +162,4 @@ def showthetop():
 
 app.config['SECRET_KEY'] = 'startrek'
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT',5000))
-	app.run(host ='0,0,0,0',port=port,debug=True)
+	app.run(host ='0,0,0,0')
